4e609bfe01dd24d2b12514ccf52cd50bfe3f1b18
TheRayhan009 <rayhanthestorymaker@gmail.com>
2024-08-19 13:01:07 +0600

    add sign and login

--- RTo_Do/urls.py
+++ RTo_Do/urls.py
@@ -22,6 +22,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.home,name="home"),
-    # path("login/",views.login,name="login"),
+    path("login/",views.login,name="login"),
     path("signin/",views.signin,name="signin"),
+    path("addtask/",views.addtask,name="addtask"),
+    path("logout/",views.logout,name="logout"),
 ]
--- RTo_Do/views.py
+++ RTo_Do/views.py
@@ -1,14 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from  datas.models import Task,Users
 
 def home(request):
+    log=request.session.get("logornot")
+    Uname=request.session.get("username")
+    passw=request.session.get("password")
     tasks=Task.objects.all()
+    if request.method=="POST":
+        add_task_button=request.POST.get("atask")
+        complet_task_button=request.POST.get("ctask")
+        delete_task_button=request.POST.get("dtask")
+        if delete_task_button=="True":
+            for i in tasks:
+                if check_task=="on":
+                    check_task=request.POST.get(f"task{i.id}")
+                    s_task=Task.objects.filter(id=i.id)
+                    s_task.delete()
+        elif complet_task_button=="True":
+            for i in tasks:
+                ##
+                if check_task=="on":
+                    check_task=request.POST.get(f"task{i.id}")
+                    s_task=Task.objects.filter(id=i.id)
+                    s_task.delete()
+        elif add_task_button=="True":
+            return redirect("/addtask")
+        return redirect("/")
     ele={
         "tasks":tasks,
+        "log":log,
+        "Uname":Uname,
+        "passw":passw,
     }
     return render(request, 'home.html',ele)
 
+def addtask(request):
+    if  request.method=="POST":
+        task_name=request.POST.get("taskname")
+
+    return render(request,"addtask.html")
+
 def signin(request):
     if request.method=="POST":
-        pass
+        fname=request.POST.get("first_name")
+        lname=request.POST.get("last_name")
+        uname=request.POST.get("username")
+        upass=request.POST.get("password")
+        
+        data=Users(
+            Fname=fname,
+            Lname=lname,
+            username=uname,
+            password=upass
+        )
+        data.save()
+        return redirect("/login")
     return render(request,"signin.html")
+
+def login(request):
+    if request.method=="POST":
+        logornot=True
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        chak=Users.objects.get(username=username,password=password)
+        if chak:
+            request.session["username"] = username
+            request.session["password"] = password
+            request.session["logornot"] = logornot
+        return redirect("/")
+    return render(request,"login.html") 
+
+def logout(request):
+    request.session.flush()
+    return redirect("/")
