0e0531f84207e7a6e36f02aac6dfc597131b59a2
TheRayhan009 <rayhanthestorymaker@gmail.com>
2024-08-20 23:45:52 +0600

    hendeling errors and add meany things

--- RTo_Do/urls.py
+++ RTo_Do/urls.py
@@ -22,8 +22,11 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.home,name="home"),
+    path("about/",views.about,name="about"),
     path("login/",views.login,name="login"),
     path("signin/",views.signin,name="signin"),
     path("addtask/",views.addtask,name="addtask"),
     path("logout/",views.logout,name="logout"),
+    path("edittask/",views.edittask,name="edittask"),
+    # path("search/",views.search,name="search"),
 ]
--- RTo_Do/views.py
+++ RTo_Do/views.py
@@ -11,6 +11,8 @@ def home(request):
             add_task_button=request.POST.get("atask")
             complet_task_button=request.POST.get("ctask")
             delete_task_button=request.POST.get("dtask")
+            edit_task_button=request.POST.get("etask")
+            search_task_button=request.POST.get("searchbtn")
             if delete_task_button=="True":
                 for i in tasks:
                     check_task=request.POST.get(f"task{i.id}")
@@ -37,6 +39,49 @@ def home(request):
                 data.T_task=x+1
                 data.save()
                 return redirect("/addtask")
+            elif search_task_button=="True":
+                search_task=request.POST.get("UTsearch")
+                result=Task.objects.filter(user_task__icontains=search_task)
+                pintask = Users.objects.get(username=Uname).T_task - Users.objects.get(username=Uname).C_task
+                ele={
+                    "tasks":result,
+                    "log":log,
+                    "Uname":Uname,
+                    "passw":passw,
+                    "num_of_C_task":Users.objects.get(username=Uname).C_task,
+                    "numtasks":Users.objects.get(username=Uname).T_task,
+                    "pintask":pintask,
+                }
+                return render(request, 'home.html',ele)
+            elif edit_task_button=="True":
+                v=[]
+                for i in tasks:
+                    check_task=request.POST.get(f"task{i.id}")
+                    if check_task=="on":
+                        v.append(i.id)
+                if len(v)==1:
+                    founded_Task=Task.objects.get(id=v[0])
+                    ele={
+                        "main_task":founded_Task.user_task,
+                        "log":log,
+                        "Uname":Uname,
+                        "passw":passw,
+                        "task_id":v[0],
+                    }
+                    return render(request,"edit.html",ele)
+                else:
+                    pintask = Users.objects.get(username=Uname).T_task - Users.objects.get(username=Uname).C_task
+                    ele={
+                        "tasks":Task.objects.filter(task_user_name=Uname),
+                        "log":log,
+                        "Uname":Uname,
+                        "passw":passw,
+                        "num_of_C_task":Users.objects.get(username=Uname).C_task,
+                        "numtasks":Users.objects.get(username=Uname).T_task,
+                        "pintask":pintask,
+                        "ala":True,
+                    }
+                    return render(request, 'home.html',ele)
             return redirect("/")
         pintask = Users.objects.get(username=Uname).T_task - Users.objects.get(username=Uname).C_task
         ele={
@@ -47,13 +92,26 @@ def home(request):
             "num_of_C_task":Users.objects.get(username=Uname).C_task,
             "numtasks":Users.objects.get(username=Uname).T_task,
             "pintask":pintask,
-
         }
         return render(request, 'home.html',ele)
     else:
         return render(request, 'home.html')
 
+def about(request):
+    Uname=request.session.get("username")
+    passw=request.session.get("password")
+    log=request.session.get("logornot")
+    ele={
+        "log":log,
+        "Uname":Uname,
+        "passw":passw,
+    }
+    return render(request,"about.html",ele)
+
 def addtask(request):
+    Uname=request.session.get("username")
+    passw=request.session.get("password")
+    log=request.session.get("logornot")
     if  request.method=="POST":
         User_task=request.POST.get("user_task")
         uname=request.session.get("username")
@@ -63,8 +121,12 @@ def addtask(request):
         )
         data.save()
         return redirect("/")
-
-    return render(request,"addtask.html")
+    ele={
+        "log":log,
+        "Uname":Uname,
+        "passw":passw,
+    }
+    return render(request,"addtask.html",ele)
 
 def signin(request):
     if request.method=="POST":
@@ -84,23 +146,37 @@ def signin(request):
     return render(request,"signin.html")
 
 def login(request):
-    if request.method=="POST"          :
+    if request.method=="POST":
         logornot=True
         username=request.POST.get("username")
         password=request.POST.get("password")
-        chak=Users.objects.get(username=username,password=password)
-        num_of_C_task=0                  
-        request.session["cmptask"] = num_of_C_task
+        chak=Users.objects.filter(username=username,password=password)
         if chak:
-            request.session["username"] =  username
-            request.session["password"] = password
-            request.session["logornot"] = logornot
-        return redirect("/")
+            num_of_C_task=0                  
+            request.session["cmptask"] = num_of_C_task
+            if chak:
+                request.session["username"] =  username
+                request.session["password"] = password
+                request.session["logornot"] = logornot
+            return redirect("/")
+        else:
+            return redirect("/login")
     return render(request,"login.html") 
+
+def edittask(request):
+    if request.method=="POST":
+        UP_task=request.POST.get("user_edited_task")
+        u_task_id=request.POST.get("task_id")
+        data=Task.objects.get(id=u_task_id)
+        data.user_task=UP_task
+        data.save()
+        return redirect("/")
+    return render(request,"edit.html")
+
 
 def logout(request):
     Uname=request.session.get("username" )
-    data=Task.objects.filter(task_user_name=Uname  )
+    data=Task.objects.filter(task_user_name=Uname)
     data.delete()
     data=Users.objects.get(username=Uname)
     data.T_task=0
